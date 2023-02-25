ddcc5b80d514ccda08186ccc004696dc14b15c83
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-25 15:18:01 -0300

    Inicia book-web-service

---
+++ src/test/java/com/application/bookwebservice/BookWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.bookwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
